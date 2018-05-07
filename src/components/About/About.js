76282634e28acb9761e4cb3500d5fa21fd3ff1e5
g1st <gintstan@gmail.com>
2018-05-07 15:15:47 +0100

    finished About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,12 +6,14 @@ import classes from './About.css';
 const about = () => (
   <section className={classes.About}>
     <Element name="scrollToAbout" />
-    <div>About Our Agency</div>
-    <div>
-      We are a new design studio based in USA. We have over 20 years of combined
-      experience, and know a thing or two about designing websites and mobile
-      apps. Clever use of technology and lean processes enable us to work faster
-      and smarter.
+    <div className={classes.Wrapper}>
+      <div className={classes.Heading}>About Our Agency</div>
+      <p className={classes.Paragraph}>
+        We are a new design studio based in USA. We have over 20 years of
+        combined experience, and know a thing or two about designing websites
+        and mobile apps. Clever use of technology and lean processes enable us
+        to work faster and smarter.
+      </p>
     </div>
   </section>
 );
