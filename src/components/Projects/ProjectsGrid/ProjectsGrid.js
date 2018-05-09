db59dd564e15241b47c78bccd0980a968aee0937
g1st <gintstan@gmail.com>
2018-05-09 20:33:21 +0100

    projects section completed

--- src/components/Projects/ProjectsGrid/ProjectsGrid.js
+++ src/components/Projects/ProjectsGrid/ProjectsGrid.js
@@ -5,19 +5,60 @@ import classes from './ProjectsGrid.css';
 const projectsGrid = () => (
   <div className={classes.ProjectsGrid}>
     <div className={classes.Container}>
-      <div className={classes.Item}>
-        <div className={classes.Project}>
-          <div>Axujiens projects</div>
+      <div className={[classes.Item1, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
         </div>
       </div>
-      <div className={classes.Item}>Item</div>
-      <div className={classes.Item}>Item</div>
-      <div className={classes.Item}>Item</div>
-      <div className={classes.Item}>Item</div>
-      <div className={classes.Item}>Item</div>
-      <div className={classes.Item}>Item</div>
-      <div className={classes.Item}>Item</div>
-      <div className={classes.Item}>Item</div>
+      <div className={[classes.Item2, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
+      <div className={[classes.Item3, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
+      <div className={[classes.Item4, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
+      <div className={[classes.Item5, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
+      <div className={[classes.Item6, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
+      <div className={[classes.Item7, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
+      <div className={[classes.Item8, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
+      <div className={[classes.Item9, classes.Dark].join(' ')}>
+        <div className={classes.Overlay}>
+          <h1 className={classes.Heading}>Lorem Ipsum</h1>
+          <h3 className={classes.Info}>Mockup</h3>
+        </div>
+      </div>
     </div>
   </div>
 );
